909b555f2ad519cf7164e05d45f7151a3ed79854
Nikkitajha <149227732+Nikkitajha@users.noreply.github.com>
2026-09-09 22:56:42 +0530

    Update RiskController.java

--- src/main/java/com/internshiproject/portfolioManagement/controller/RiskController.java
+++ src/main/java/com/internshiproject/portfolioManagement/controller/RiskController.java
@@ -2,28 +2,41 @@ package com.internshiproject.portfolioManagement.controller;
 
 import com.internshiproject.portfolioManagement.entity.User;
 import com.internshiproject.portfolioManagement.repository.UserRepository;
+import com.internshiproject.portfolioManagement.service.JwtService;
 import com.internshiproject.portfolioManagement.service.RiskService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/risk")
 @CrossOrigin("*")
-    public class RiskController {
+public class RiskController {
 
-        @Autowired
-        private RiskService riskService;
+    @Autowired
+    private RiskService riskService;
 
-        @Autowired
-        private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-        @GetMapping
-        public String checkRisk(@RequestParam String email) {
+    @Autowired
+    private JwtService jwtService;
 
-            User user = userRepository.findByEmail(email)
-                    .orElseThrow(() -> new RuntimeException("User not found"));
+    @GetMapping
+    public String checkRisk(
+            @RequestHeader("Authorization") String token) {
 
-            return riskService.checkRisk(user);
-        }
+        String email =
+                jwtService.extractUsername(
+                        token.replace("Bearer ", "")
+                );
+
+        User user =
+                userRepository.findByEmail(email)
+                        .orElseThrow(
+                                () -> new RuntimeException("User not found")
+                        );
+
+        return riskService.checkRisk(user);
     }
-
+}
