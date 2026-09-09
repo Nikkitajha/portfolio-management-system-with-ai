71fe5d4609f131fd9bcdfa2ae2141e405705b638
Nikkitajha <149227732+Nikkitajha@users.noreply.github.com>
2026-09-09 22:31:04 +0530

    Update StockRepository.java

--- src/main/java/com/internshiproject/portfolioManagement/repository/StockRepository.java
+++ src/main/java/com/internshiproject/portfolioManagement/repository/StockRepository.java
@@ -2,12 +2,23 @@ package com.internshiproject.portfolioManagement.repository;
 
 import com.internshiproject.portfolioManagement.entity.Stock;
 import com.internshiproject.portfolioManagement.entity.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     List<Stock> findByUser(User user);
-    Stock findByUserAndSymbol(User user, String symbol);
+
+    Stock findByUserAndSymbol(
+            User user,
+            String symbol
+    );
+
+    Optional<Stock> findByIdAndUser(
+            Long id,
+            User user
+    );
 }
